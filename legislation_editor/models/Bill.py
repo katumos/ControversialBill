4f3a5b0ff95db2c145b9d988739131b11d76ab05
unknown <Ben Willard@.(none)>
2012-03-19 06:48:11 -0500

    updated .plan with some goals and accomplishments and added a max_length to Bill model

--- legislation_editor/models/Bill.py
+++ legislation_editor/models/Bill.py
@@ -3,7 +3,7 @@ from django.db import models
 from djangotoolbox.fields import ListField
 
 class Bill(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=32)
     body = models.TextField()
     sections = ListField()
     comments = ListField()
