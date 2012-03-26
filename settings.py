8cc909b230246828e6c1d61a51fb5d46ccc7dd35
Valis C <valis@inkanito.net>
2012-03-26 00:28:09 -0700

    Going back to just the Django-orm. Removing Mongodb. Ignoring transient sqlite db file.

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # Django settings for ControversialBill project.
+from os import path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -10,9 +11,13 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default' : {
-        'ENGINE' : 'django_mongodb_engine',
-        'NAME' : 'controversialbill'
+    'default': {
+        'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': path.join(path.dirname(__file__),'dev.db'),  # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
